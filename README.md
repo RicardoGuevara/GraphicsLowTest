8eff3a455673d77bedecf99a847e9bc7a385b47e
Ricardo Guevara <ricardogp@protonmail.com>
2019-07-08 01:15:48 -0500

    from scratch module initialization

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# GraphicsLowTest
-a little bit of graphic engines fundamentals
+<div>
+
+<h1 aling="center">LITTLE GRAPHIC ENGINE EXPERIMENTS</h1>
+<p>
+All the following tests was made in order to improove C++ skills 
+and get some knowledge about graphic engines generic fundamentals, 
+i'm not an expert, if you came over here searching for pure beautifull 
+code practices, i'm so sorry, be pattient and please put all your furious
+developer rage on a code review. Thanks
+</p>
+
+<h2>How to test it</h2>
+<p>
+Personally, i used MinGW (wich allows you to have common GNU C and C++ compilers)
+to get the gcc compiler, so i compile directly from prompt (or any terminal) using<br>
+<code>>> gcc ScriptName.cpp<code><br>
+you can also use code::blocks or any C++ IDE to make it easier.
+</p>
+
+<h2>The "scratch" game engine:</h2>
+<p>
+at least an 80% of the theory, code fundamentals (and the header...) belongs to a series of game engine theory and applications by javidx9, found him at:<br>
+https://www.youtube.com/channel/UC-yuWVUplUJZvieEligKBkA <br>
+https://github.com/OneLoneCoder
+</p>
+
+
+</div>
---
+++ scratch/graphicstester.cpp
@@ -0,0 +1,112 @@
+#include "scratchGameEngine.h"
+using namespace std;
+
+//vector axis structure to represent a corner
+struct vec3d
+{
+    float x, y, z; 
+};
+
+struct triangle
+{
+    vect3d p[3];
+};
+
+struct mesh
+{
+    vector<triangle> tris;
+};
+
+struct mat4x4
+{
+    float m[4][4] = { 0 };
+};
+
+class engine3D : public scratchGameEngine
+{
+    public:
+    engine3D()
+    {
+        m_sAppName = L"3D Demo";
+    }
+
+    private:
+    mesh meshCube;
+    mat4x4 matProj;
+
+    float fTheta;
+
+	//matrix multiply
+
+
+    public:
+    bool OnUserCreate() override
+    {
+        meshCube.tris = {
+
+		// SOUTH
+		{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+		{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+		// EAST                                                      
+		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+		// NORTH                                                     
+		{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+		{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+		// WEST                                                      
+		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+		// TOP                                                       
+		{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+		{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+		// BOTTOM                                                    
+		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+		};
+
+
+        float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = 0.0f;
+
+        return true;
+    }
+
+    public:
+    bool OnUserUpdate() override
+    {
+        Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+        for(auto tri : meshCube.tris)
+        {
+
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    engine3D demo;
+    if(demo.ConstructConsole(256, 240, 4,4))
+        demo.Start();
+
+
+    return 0;
+}
